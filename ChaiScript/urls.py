b1d4e5d0cd9fefe335daf0e38d19578272fef826
Zishan108 <zishan.dev15@gmail.com>
2026-04-19 13:47:28 +0530

    remove temp superuser route

--- ChaiScript/urls.py
+++ ChaiScript/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.models import User
 from engine import views as engine_views
 
 handler404 = 'engine.views.custom_404'
@@ -13,12 +11,6 @@ def docs(request):
 def downloads(request):
     return render(request, "engine/downloads.html")
 
-def create_super(request):
-    if not User.objects.filter(username='kami').exists():
-        User.objects.create_superuser('kami', 'zishan@example.com', 'Kami@1234')
-        return HttpResponse('Superuser created!')
-    return HttpResponse('Already exists!')
-
 urlpatterns = [
     path("",           engine_views.home,         name="home"),
     path("admin/",     admin.site.urls),
@@ -26,5 +18,4 @@ urlpatterns = [
     path("docs/",      docs,                      name="docs"),
     path("downloads/", downloads,                 name="downloads"),
     path("accounts/",  include("allauth.urls")),
-    path("init-super/", create_super),
 ]
